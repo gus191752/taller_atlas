230dd45d74c1d464839fe51496cdb47bcf5f29ad
gustavo adolfo mujica perez <122643433+gus191752@users.noreply.github.com>
2023-07-17 21:12:42 -0400

    cambios 17

--- js/animacion.js
+++ js/animacion.js
@@ -1,1 +1,23 @@
+///////////////////////////////menu hamburguesa///////////////////////////////////////////////
+const contenedorBoton = document.querySelector(".contenedor-boton");
+const contenedorMenu = document.querySelector(".contenedor-menu");
+
+contenedorBoton.addEventListener("click", () => {
+  contenedorMenu.classList.toggle("contenedor-menu_visible");
+  if (contenedorMenu.classList.contains("contenedor-menu_visible")) {
+    contenedorBoton.setAttribute("aria-label", "Cerrar menú");
+  } else {
+    contenedorBoton.setAttribute("aria-label", "Abrir menú");
+  }
+});
+
+const contenedormenuLinks = document.querySelectorAll(
+  '.contenedor-menu a[href^="#"]'
+);
+
+contenedormenuLinks.forEach((contenedormenuLink) => {
+  contenedormenuLink.addEventListener("click", function () {
+    contenedorMenu.classList.remove("contenedor-menu_visible");
+  });
+});
 
