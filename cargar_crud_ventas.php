dccd029a5a0d28c35b74102e8dfe944cbd1703c9
gustavo adolfo mujica perez <122643433+gus191752@users.noreply.github.com>
2023-08-12 14:20:54 -0400

    c50

--- cargar_crud_ventas.php
+++ cargar_crud_ventas.php
@@ -5,28 +5,37 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Administrador de ventas</title>
-    <link rel="stylesheet" type="text/css" href="css/estilos.css">
-	<script src="https://kit.fontawesome.com/779ae99c75.js" crossorigin="anonymous"></script>
-
+	  <script src="https://kit.fontawesome.com/779ae99c75.js" crossorigin="anonymous"></script>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-+0n0xVW2eSR5OomGNYDnhzAbDsOXxcvSN1TPprVMTNDbiYZCxYbOOl7+AMvyTG2x" crossorigin="anonymous">
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.1/dist/js/bootstrap.bundle.min.js" integrity="sha384-gtEjrD/SeCtmISkJkNUaaKMoLD0//ElJ19smozuHV6z3Iehds+3Ulb9Bn9Plx0x4" crossorigin="anonymous"></script>
     <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.9.2/dist/umd/popper.min.js" integrity="sha384-IQsoLXl5PILFhosVNubq5LC7Qb9DXgDA9i+tQ8Zj3iwWAwPtgFTxbJ8NT4GN1R8p" crossorigin="anonymous"></script>
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.1/dist/js/bootstrap.min.js" integrity="sha384-Atwg2Pkwv9vp0ygtn1JAojH0nYbwNJLPhwyoVbhoPwBhjQPR5VtM2+xf0Uwh9KtT" crossorigin="anonymous"></script>
     <script src="https://code.jquery.com/jquery-3.6.0.min.js" integrity="sha256-/xUj+3OJU5yExlq6GSYGSHk7tPXikynS7ogEvDej/m4=" crossorigin="anonymous"></script>
-
+    <link rel="stylesheet" type="text/css" href="css/estilos_venta.css">
 </head>
 
-
 <body>
+
   <section id="contenedor_pag">
   <div class="titulo_pag">
-    <h1><i class="fa-solid fa-box"></i>Ventas</h1>
-
+    <h1><i class="fa-solid fa-box"></i>   Ventas</h1>
   </div>
-
+  <div class="datos_cliente">
+    <div class="accion_cliente">
+      <h1>Datos del Cliente</h1>
+      <br>
+      <br>
+      <thead>
+        <th><input type="text" REQUIRED name="cedula" placeholder="Cedula..." value="" id="cedula"  onblur="buscar_datos_tabla_trabajadores();"><br><br></th>
+        <td><input type="text"  name="trabajador" placeholder="Nombre de Trabajador..." id="vendedor" ><br><br></td>
+        <th><input   type="text" REQUIRED name="codigo" placeholder="Codigo..." value="" id="codigo" onblur="buscar_datos_tabla_productos();"><br><br></th>
+        
+      </thead>
+    </div>
+  </div>
   </section>
 
-    <h1>Administrador de Ventas</h1>
+    
     <div>
         <form ><br><br>
         <input type="text" REQUIRED name="cedula" placeholder="Cedula..." value="" id="cedula"  onblur="buscar_datos_tabla_trabajadores();"><br><br>
@@ -44,12 +53,10 @@
     <br>
     <a href="administrador.html">Volver</a>
 <br>
+
 <script>
    function buscar_datos_tabla_trabajadores()
 {
-    //cedula = $("#cedula").val();
-    
-    
     var parametros = 
     {
       "buscar_cedula": "1",
@@ -81,9 +88,6 @@
 } 
 function buscar_datos_tabla_productos()
 {
-    //cedula = $("#cedula").val();
-    
-    
     var parametros = 
     {
       "buscar_producto": "1",
