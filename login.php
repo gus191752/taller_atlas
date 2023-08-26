e02e1d64c8db708a95a2f4446e08ec128cdad38d
gustavo adolfo mujica perez <122643433+gus191752@users.noreply.github.com>
2023-08-26 15:19:40 -0400

    a1

--- login.php
+++ login.php
@@ -1,22 +1,59 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>login taller atlas</title>
+   <meta charset="utf-8">
+   <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+   <link rel="stylesheet" type="text/css" href="css/estilos.css">
+ 
+   <title>Inicio de sesión</title>
 </head>
+
 <body>
-    
-    <form action="validar_login.php" method="post">
-    <h1>Sistema de Login </h1>
-    <p>Usuario <input type="text" placeholder="Ingrese usuario o correo" name="nombre"> </p>
-    <p>Contraseña <input type="text" placeholder="Ingrese contraseña" name="contraseña">
-    <br>
-    <br>
-    <input type="submit" value="Ingresar">    
-</form>
+   
+   <div class="container">
+      <div class="img">
+         <img height="120px" width="120px" src="imagenes/logo redondo en formato png.png">
+      </div>
+      <div class="login-content">
+         <form method="post" action="">
+            <img height="50px" width="50px" src="imagenes/vehiculo.png">
+            <h2 class="title">BIENVENIDO</h2>
+            <div class="input-div one">
+               <div class="i">
+                  <i class="fas fa-user"></i>
+               </div>
+               <div class="div">
+                  <h5>Usuario</h5>
+                  <input id="usuario" type="text" class="input" name="usuario">
+               </div>
+            </div>
+            <div class="input-div pass">
+               <div class="i">
+                  <i class="fas fa-lock"></i>
+               </div>
+               <div class="div">
+                  <h5>Contraseña</h5>
+                  <input type="password" id="input" class="input" name="password">
+               </div>
+            </div>
+            <div class="view">
+               <div class="fas fa-eye verPassword" onclick="vista()" id="verPassword"></div>
+            </div>
+
+            <div class="text-center">
+               <a class="font-italic isai5" href="">Olvidé mi contraseña</a>
+               <a class="font-italic isai5" href="">Registrarse</a>
+            </div>
+            <input name="btningresar" class="btn" type="submit" value="INICIAR SESION">
+         </form>
+      </div>
+   </div>
+  
 
 </body>
+
 </html>
 
 
