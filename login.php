745555232a39e94f0149e895ec8a7bbc9a85b6af
gustavo adolfo mujica perez <122643433+gus191752@users.noreply.github.com>
2023-08-30 19:06:01 -0400

    v3

--- login.php
+++ login.php
@@ -5,7 +5,7 @@
    <meta charset="utf-8">
    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
 
-   <link rel="stylesheet" type="text/css" href="css/estilos.css">
+   <link rel="stylesheet" type="text/css" href="css/estilos_login.css">
  
    <title>Inicio de sesión</title>
 </head>
