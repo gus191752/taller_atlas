4accca45b6d860f4964768e8c854c159351154b3
gus191752 <gus191752@gmail.com>
2024-05-26 16:38:28 -0400

    comen

--- administrador.php
+++ administrador.php
@@ -70,7 +70,7 @@ if ($_SESSION['jerarquia']!='9')
     <footer class="footer" id="contacto">
         <h4>Soluciones gm 83 c.a.</h4>
         <h4>Rif J-50411101-5</h4>
-        <h4>Centro Comercial La Romana local B4, av Miranda c/c av ayacucho, Maracay</h4>
+        <h4>Centro Comercial La Romana local B4 y B5, av Miranda c/c av ayacucho, Maracay</h4>
         <h4>tlf: 0412-147.19.97</h4>
         <h4>Correo: talleratlasmcy@gmail.com</h4>
         <h4>Instagram: @talleratlasmcy</h4>
